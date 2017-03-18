b4005dafaa752b73806bc199d8fcc138febd53f0
WillWu <wjxzju@163.com>
2017-03-18 21:24:43 +0800

    Update BubbleSort.cpp

--- Data structure/sort/BubbleSort.cpp
+++ Data structure/sort/BubbleSort.cpp
@@ -1,28 +1,34 @@
+
 #include <iostream>
 using namespace std;
 
-void bubblesort(int a[],int size)
+void bubblesort(int a[], int size)
 {
-	for (int i = size-1; i >=0; i--)
+	for (int i = size - 1; i >= 0; i--)
 	{
-		int flag = 1;
-		for (int j = 0; j < i ; j++)
+		bool flag = 1;
+		for (int j = 0; j < i; j++)
 		{
-			if (a[j]>a[j + 1]){ int  temp = a[j]; a[j] = a[j + 1]; a[j + 1] = temp; flag = 0; }
+			if (a[j]>a[j + 1]) { 
+				int  temp = a[j]; a[j] = a[j + 1]; a[j + 1] = temp; 
+				flag = 0;
+			}
 		}
 		if (flag)
 			break;
+		
 	}
 }
 int main(void)
 {
-	int a[] = { 10, 7, 9, 6, 4, 5, 3, 2, 1, 8 };
-	int size = 10;
-	bubblesort(a,10);
+	int a[] = { 5,4,2,3,1,6,7,8,9 };
+	int size = 9;
+	bubblesort(a, 9);
 	cout << "----bubble sort----" << endl;
-	for (int  i = 0; i <10; i++)
+	for (int i = 0; i <9; i++)
 	{
 		cout << a[i] << endl;
 	}
+	system("pause");
 	return 0;
 }
